25c9280cd9a482be37f9c1e6ebcf2a7e26b9b203
lazabou <10668741+lazabou@users.noreply.github.com>
2021-01-12 22:34:10 +0100

    mess

--- CreateTopo.py
+++ CreateTopo.py
@@ -1,21 +1,36 @@
 import ModuleTopo as Topo
 import yaml
 
-print("choose your action:")
-print("1. Add a Router")
-print("2. Add an Interface to a router")
-Choice = input()
+while True:
+  print("choose your action:")
+  print("1. Add a Router")
+  print("2. Add a link")
+  print("3. Quit")
+  Choice = input()
 
-print("Your Choice is :")
-print(Choice)
-
-if Choice=='1':
-  Id=int(input("Router Id (1-253):"))
-  if Id>253 or Id<1: print("wrong id")
-  else :
-    HostName=input("Router HostName:")
-    Answer=Topo.AddRouter(Id,HostName)
-    print(Answer)
-
-else: 
-  print("invalid choice")
+  if Choice=='1':
+    Id=int(input("Router Id (1-253):"))
+    if Id>253 or Id<1: print("wrong id")
+    else :
+      HostName=input("Router HostName:")
+      Answer=Topo.AddRouter(Id,HostName)
+      print(Answer)
+  elif Choice=='2':
+    SrcId=int(input("Source Router Id (1-253):"))
+    DstId=int(input("Destination Router Id (1-253):"))
+    SrcIf=int(input("Source Router Interface (0-6):"))
+    DstIf=int(input("Destination Router Interface (0-6):"))
+    while True:
+      IsCore=str(input("MPLS interface? y/n:"))
+      if IsCore=="y" or IsCore=="n":
+        break
+      else : print("invalid choice")
+    if SrcId==DstId and SrcIf==DstIf:
+      print("error: same source/destination router id and same source/destination router interface")
+    else:
+      Answer=Topo.AddLink(SrcId,SrcIf,DstId,DstIf,IsCore)
+      print(Answer)
+  elif Choice=='3':
+    break
+  else: 
+    print("invalid choice")
--- ModuleTopo.py
+++ ModuleTopo.py
@@ -6,33 +6,41 @@ def AddRouter(Id,HostName):
   with open("Topology.yaml", 'r') as yamlfile:
     Devices = yaml.load(yamlfile,Loader=yaml.FullLoader)
 
+  #create dictionnary with default values
   NewRouter = {
     Id: {
       'HostName': HostName,
-      'MgmtIP': '127.0.0.1',
       'NetconfPort': 830,
-      'Interfaces': {
-        'Name': 'ge-0/0/0',
-        'ConnectedTo': 'None',
-        'Core': False
-       }
+      'Interfaces':{
+        'lo0':{
+          'IP':"10.0.0."+str(Id),
+        }
+      }
     }
   }
-  
-  
+
+  #if current topo file is empty
   if Devices == None:
     Devices=NewRouter
     with open("Topology.yaml", 'w') as yamlfile:
         data = yaml.dump(Devices, yamlfile, explicit_start = True)
     return("Device Created")
+  
+  #check if router is already in topo file
   else :
     DeviceExists = False
     for x in Devices.keys(): 
       if x == Id:
         DeviceExists = True
   
+    #if yes, update hostname
     if DeviceExists == True:
-      return("This device already exists")
+      Devices[Id]["HostName"]=HostName
+      with open("Topology.yaml", 'w') as yamlfile:
+        data = yaml.dump(Devices, yamlfile, explicit_start = True)
+      return("This device already exists, hostname updated")
+    
+    #if not, add new router to topo
     else:
       Devices[Id]=NewRouter[Id]
       with open("Topology.yaml", 'w') as yamlfile:
@@ -40,5 +48,73 @@ def AddRouter(Id,HostName):
       return("Device Created")
 
 
- 
-    
+def AddLink(SrcId,SrcIf,DstId,DstIf,IsCore):
+  #get current topology
+  with open("Topology.yaml", 'r') as yamlfile:
+    Devices = yaml.load(yamlfile,Loader=yaml.FullLoader)
+  
+  #create src-interface
+  SrcIfName='ge-0/0/'+str(SrcIf)
+  if IsCore == 'y':
+    if int(SrcId)<int(DstId):
+      IP='10.'+str(SrcId)+'.'+str(DstId)+'.'+str(SrcId)+'/24'
+    else:
+      IP='10.'+str(DstId)+'.'+str(SrcId)+'.'+str(SrcId)+'/24'
+  
+    SrcInterface= {
+      SrcIfName: {
+        'ConnectedTo': DstId,
+        'MPLS': IsCore,
+        'IP':IP
+      }
+    }
+  else:
+        SrcInterface= {
+      SrcIfName: {
+        'ConnectedTo': DstId,
+        'MPLS': IsCore,
+      }
+    }
+
+  #create dst-interface
+  DstIfName='ge-0/0/'+str(DstIf)
+  if IsCore == 'y':
+    if int(SrcId)<int(DstId):
+      IP='10.'+str(SrcId)+'.'+str(DstId)+'.'+str(DstId)+'/24'
+    else:
+      IP='10.'+str(DstId)+'.'+str(SrcId)+'.'+str(DstId)+'/24'
+
+    DstInterface= {
+      DstIfName: {
+        'ConnectedTo': SrcId,
+        'MPLS': IsCore,
+        'IP':IP
+      }
+    }
+  else:
+    DstInterface= {
+      DstIfName: {
+        'ConnectedTo': SrcId,
+        'MPLS': IsCore,
+      }
+    }
+
+  #check if routers are already in topo file
+  SrcDeviceExists = False
+  for x in Devices.keys(): 
+    if x == SrcId:
+      SrcDeviceExists = True
+  
+  DstDeviceExists = False
+  for x in Devices.keys(): 
+    if x == SrcId:
+      DstDeviceExists = True
+
+  if SrcDeviceExists==False or DstDeviceExists == False:
+    print("Router does not exist")
+  else:
+   Devices[SrcId]["Interfaces"][SrcIfName]=SrcInterface[SrcIfName]
+   Devices[DstId]["Interfaces"][DstIfName]=DstInterface[DstIfName]
+   with open("Topology.yaml", 'w') as yamlfile:
+    data = yaml.dump(Devices, yamlfile, explicit_start = True)
+   return("Link Created")
