a857485deefde0d68b49cce8c71d8b3b8355718e
adesportes <adrien_desportes@yahoo.fr>
2021-01-20 22:47:39 +0100

    Removing test interim scripts (*test*) Finalyzing push_conf script Changing jinja2 template which has an error

--- Push_Conf.py
+++ Push_Conf.py
@@ -2,13 +2,24 @@ import yaml
 import sys
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import ConnectError
+from jnpr.junos.exception import LockError
+from jnpr.junos.exception import UnlockError
+from jnpr.junos.exception import ConfigLoadError
+from jnpr.junos.exception import CommitError
 
-login = "test"
-passwd = "Juniper123"
+# When calling the script type:
+# - as first argument: device login
+# - as second argument: device password
+login = sys.argv[1]
+passwd = sys.argv[2]
 
+# Parse Topology file to connect to each device
 with open("Topology.yaml", 'r') as yamlfile:
   Devices = yaml.load(yamlfile,Loader=yaml.FullLoader)
 
+# Parse device dictionnary ie Topology file to map each device with JCL IP and Netconf port
 for Key, Router in Devices.items():
   print ('Je vais prendre la conf du routeur' + str(Key))
   print ("Quelle IP pour le "+(Router['HostName']) + "?")
@@ -17,13 +28,34 @@ for Key, Router in Devices.items():
   if not (Port):
       Port='830'
   dev = Device(host=IP, user=login, passwd=passwd, port=Port)
+
+# Then connect to each IP mapped to the device to load relevant config file which is in /Config folder
   try:
     dev.open()
   except ConnectError as err:
     print ("Cannot connect to device: {0}".format(err))
     sys.exit(1)
-#  except Exception as err:
-#      print (err)
-#      sys.exit(1)
-  print (dev.facts)
-  dev.close()
+  else:
+    cu = Config(dev)
+    print ("Loading configuration changes for %s" %str(Key))
+    try:
+        cu.load(path="configs/"+str(Key)+".txt", merge=True)
+        cu.pdiff()
+    except:
+        print ("Unable to load configuration changes: {0}".format(err))
+    finally:
+        dev.close()
+
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
